637e7de98d82f379d4f9120899d99212b9a38d41
Гальцев Илья <ilyandreev771@gmail.com~>
2022-12-14 20:57:50 +0300

    1 commit

---
+++ src/assets/data/massConspects.js
@@ -0,0 +1,29 @@
+const massConspects = [
+  {
+    id: 1,
+    title: '',
+    path: '',
+  },
+  {
+    id: 2,
+    title: '',
+    path: '',
+  },
+  {
+    id: 3,
+    title: '',
+    path: '',
+  },
+  {
+    id: 4,
+    title: '',
+    path: '',
+  },
+  {
+    id: 5,
+    title: '',
+    path: '',
+  },
+]
+
+export default massConspects;
