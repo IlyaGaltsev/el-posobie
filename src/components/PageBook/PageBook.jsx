bdc6c8e542bcc0d2503de9c421d704459de8c57f
Гальцев Илья <ilyandreev771@gmail.com~>
2022-12-18 17:27:59 +0300

    main page

---
+++ src/components/PageBook/PageBook.jsx
@@ -0,0 +1,17 @@
+import "./PageBook.scss";
+
+const PageBook = (props) => {
+  const { id, timeRead, title, text, onNext } = props;
+
+  return (
+    <div className="pagebook">
+      <div className="pagebook__wrapper">
+        <span>Читается за {timeRead} минут</span>
+        <h1>{title}</h1>
+        {text()}
+        <button onClick={onNext}>Далее</button>
+      </div>
+    </div>
+  );
+};
+export { PageBook };
