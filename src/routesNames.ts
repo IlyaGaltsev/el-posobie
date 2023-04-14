b3613e4b329bb528ef5c922a1dd06ae72cf932d2
Ilya Galtsev <ilyandreev771@gmail.com>
2023-04-14 19:44:34 +0300

    добавление титульной страницы

--- src/routesNames.ts
+++ src/routesNames.ts
@@ -1,4 +1,5 @@
-export const BOOK_ROUTE: string = '/'
+export const FIRST_ROUTE: string = '/'
+export const BOOK_ROUTE: string = '/book'
 export const VIDEO_ROUTE: string = '/videos'
 export const PREZENTATIONS_ROUTE: string = '/prezentations'
 export const CONSPECTS_ROUTE: string = '/conspects'
